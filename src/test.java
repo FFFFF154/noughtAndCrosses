30f6143a95c48f5d75cc819a581839c226bfd960
FFFFF154 <nalekseev.154@gmail.com>
2023-03-19 15:57:55 +0300

    version 0.1.1

--- src/test.java
+++ src/test.java
@@ -7,6 +7,97 @@ public class test  {
     public static final int SIZE_DESK = 3;
     public static int[][] desk = new int[SIZE_DESK][SIZE_DESK];
 
+    public static boolean checkDraw(){
+        boolean cd = false;
+        int count = 0;
+        for (int i=0;i<SIZE_DESK;i++){
+            for (int j=0;j<SIZE_DESK;j++){
+                if (desk[i][j] != 0){
+                    count++;
+                }
+            }
+        }
+        if (count == SIZE_DESK*SIZE_DESK){
+            cd = true;
+        }
+        return cd;
+    }
+
+    public static boolean botTurnDefense(){
+        boolean k = false;
+        for (int i=0; i<SIZE_DESK-1;i++){
+            for (int j=0; j<SIZE_DESK-1; j++){
+                if (desk[i][j] == desk[i][j+1] && j == 0 && desk[i][j] != 0){
+                    desk[i][j+2] = 2;
+                    k = true;
+                    continue;
+                } else if (desk[i][j] == desk[i][j+1] && j == 1 && desk[i][j] != 0){
+                    desk[i][j-1] = 2;
+                    k = true;
+                    continue;
+                }
+                if (desk[i][j] == desk[i+1][j] && i == 0 && desk[i][j] != 0){
+                    desk[i+2][j] = 2;
+                    k = true;
+                    continue;
+                } else if(desk[i][j] == desk[i+1][j] && i == 1 && desk[i][j] != 0){
+                    desk[i-1][j] = 2;
+                    k = true;
+                    continue;
+                }
+                if (desk[i][i] == desk[i+1][i+1] && i == 0 && desk[i][i] != 0){
+                    desk[i+2][i+2] = 2;
+                    k = true;
+                    continue;
+                } else if(desk[i][i] == desk[i+1][i+1] && i == 1 && desk[i][j] != 0){
+                    desk[i-1][i-1] = 2;
+                    k = true;
+                    continue;
+                }
+                if (desk[0][2] == desk[1][1] && desk[0][2] != 0){
+                    desk[2][0] = 2;
+                    k = true;
+                    continue;
+                } else if(desk[1][1] == desk[2][0] && desk[1][1] != 0){
+                    desk[0][2] = 2;
+                    k = true;
+                    continue;
+                }
+            }
+        }
+        return k;
+    }
+
+    public static boolean botTurnCorners(){
+        boolean k = false;
+        int x2, y2;
+        if (desk[1][1] != 1 && (desk[1][1] != 2)){
+            desk[1][1] = 2;
+        }else {
+            while (true){
+                if (Math.random()*2 > 1){
+                    x2 = 2;
+                } else{
+                    x2 = 0;
+                }
+                if (Math.random()*2 > 1){
+                    y2 = 2;
+                } else{
+                    y2 = 0;
+                }
+                if (desk[x2][y2] != 1 && desk[x2][y2] != 2){
+                    desk[x2][y2] = 2;
+                    k = true;
+                    break;
+                } else {
+                    break;
+                }
+
+            }
+        }
+        return k;
+    }
+
     public static boolean checkWin(){
         boolean flag = true;
         for (int i=0; i<SIZE_DESK; i++){
@@ -29,46 +120,8 @@ public class test  {
     }
 
     public static void botTurn(){
-        int x2, y2;
-        for (int i=0; i<SIZE_DESK-1;i++){
-            for (int j=0; j<SIZE_DESK-1; j++){
-                if (desk[i][j] == desk[i][j+1] && j == 0 && desk[i][j] != 0){
-                    desk[i][j+2] = 2;
-                    break;
-                } else if (desk[i][j] == desk[i][j+1] && j == 1 && desk[i][j] != 0){
-                    desk[i][j-1] = 2;
-                    break;
-                }
-                if (desk[i][j] == desk[i+1][j] && i == 0 && desk[i][j] != 0){
-                    desk[i+2][j] = 2;
-                    break;
-                } else if(desk[i][j] == desk[i+1][j] && i == 1 && desk[i][j] != 0){
-                    desk[i-1][j] = 2;
-                    break;
-                }
-            }
-        }
-
-        if (desk[1][1] != 1 && (desk[1][1] != 2)){
-            desk[1][1] = 2;
-        } else {
-            while (true){
-                if (Math.random()*2 > 1){
-                    x2 = 2;
-                } else{
-                    x2 = 0;
-                }
-                if (Math.random()*2 > 1){
-                    y2 = 2;
-                } else{
-                    y2 = 0;
-                }
-                if (desk[x2][y2] != 1 && desk[x2][y2] != 2){
-                    desk[x2][y2] = 2;
-                    break;
-                }
-
-            }
+        if (!botTurnDefense()){
+            botTurnCorners();
         }
         printDesk();
     }
@@ -120,6 +173,9 @@ public class test  {
         while (checkWin()){
             playerTurn();
             botTurn();
+            if (checkDraw()){
+                break;
+            }
         }
         System.out.println("GAME OVER!");
         printDesk();
