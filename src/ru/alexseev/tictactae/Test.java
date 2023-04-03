3e1b062a8f61a3b45a6f1fea240c94c4d6b39094
FFFFF154 <nalekseev.154@gmail.com>
2023-04-03 09:33:21 +0300

    version 0.1.5

--- src/ru/alexseev/tictactae/Test.java
+++ src/ru/alexseev/tictactae/Test.java
@@ -21,123 +21,92 @@ public class Test {
         for (int i=0;i<SIZE_DESK;i++){
             for (int j=0;j<SIZE_DESK;j++){
                 if (desk[i][j] == ' '){
-                    return true;
+                    return false;
                 }
             }
-        }
-        return false;
-    }
-
-    public static boolean botTurnDefense(){
-        for (int i=0; i<SIZE_DESK-1;i++){
-            for (int j=0; j<SIZE_DESK-1; j++){
-                if (desk[i][j] == desk[i][j+1] && j == 0 && desk[i][j] != ' '){
-                    desk[i][j+2] = '0';
-                    return true;
-                } else if (desk[i][j] == desk[i][j+1] && j == 1 && desk[i][j] != ' '){
-                    desk[i][j-1] = '0';
-                    return true;
-                }
-                if (desk[i][j] == desk[i+1][j] && i == 0 && desk[i][j] != ' '){
-                    desk[i+2][j] = '0';
-                    return true;
-                } else if(desk[i][j] == desk[i+1][j] && i == 1 && desk[i][j] != ' '){
-                    desk[i-1][j] = '0';
-                    return true;
-                }
-                if (desk[i][i] == desk[i+1][i+1] && i == 0 && desk[i][i] != ' '){
-                    desk[i+2][i+2] = '0';
-                    return true;
-                } else if(desk[i][i] == desk[i+1][i+1] && i == 1 && desk[i][j] != ' '){
-                    desk[i-1][i-1] = '0';
-                    return true;
-                }
-                if (desk[0][2] == desk[1][1] && desk[0][2] != ' '){
-                    desk[2][0] = '0';
-                    return true;
-                } else if(desk[1][1] == desk[2][0] && desk[1][1] != ' '){
-                    desk[0][2] = '0';
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public static boolean botTurnCorners(){
-        int x2, y2;
-        if (desk[1][1] != 'X' && (desk[1][1] != '0')){
-            desk[1][1] = '0';
-        }else {
-            while (true){
-                if (Math.random()*2 > 1){
-                    x2 = 2;
-                } else{
-                    x2 = 0;
-                }
-                if (Math.random()*2 > 1){
-                    y2 = 2;
-                } else{
-                    y2 = 0;
-                }
-                if (desk[x2][y2] != 'X' && desk[x2][y2] != '0'){
-                    desk[x2][y2] = '0';
-                    return true;
-                } else {
-                    break;
-                }
-
-            }
-        }
-        return false;
-    }
-
-    public static boolean checkWin(){
-        for (int i=0; i<SIZE_DESK; i++){
-            if ((desk[i][0] == desk[i][1]) && (desk[i][0] == desk[i][2]) && (desk[i][0] != ' ')){
-                return false;
-            }
-        }
-        for (int i=0; i<SIZE_DESK; i++){
-            if ((desk[0][i] == desk[1][i]) && (desk[0][i] == desk[2][i]) && (desk[0][i] != ' ')){
-                return false;
-            }
-        }
-        if (desk[0][0] == desk[1][1] && (desk[0][0] == desk[2][2]) && (desk[0][0] != ' ')){
-            return false;
-        }
-        if (desk[2][0] == desk[1][1] && (desk[2][0] == desk[0][2]) && (desk[2][0] != ' ')){
-            return false;
         }
         return true;
     }
 
-    public static void botTurn(){
-        if (!botTurnDefense()){
-            botTurnCorners();
+    public static boolean checkWin(char sym){
+        for (int i=0; i<SIZE_DESK; i++){
+            if ((desk[i][0] == sym) && (sym == desk[i][2]) && (desk[i][1] == sym)){
+                return true;
+            }
         }
-        printDesk();
+        for (int i=0; i<SIZE_DESK; i++){
+            if ((desk[0][i] == sym) && (sym == desk[2][i]) && (desk[1][i] == sym)){
+                return true;
+            }
+        }
+        if ((desk[0][0] == sym) && (desk[1][1] == sym) && (desk[2][2] == sym)){
+            return true;
+        }
+        if ((desk[2][0] == sym) && (sym == desk[0][2]) && (desk[1][1] == sym)){
+            return true;
+        }
+        return false;
+    }
+
+    public static void botTurn(char sym1, char sym2){
+        int x2, y2;
+        if (desk[1][1] != sym1 && (desk[1][1] != sym2)){
+            desk[1][1] = sym2;
+            //System.out.println("Центр");
+        }else {
+            for (int i = 0; i < SIZE_DESK; i++) {
+                for (int j = 0; j < SIZE_DESK; j++) {
+                    if ((desk[i][j] != sym1) && (desk[i][j] != sym2)) {
+                        desk[i][j] = sym2;
+                        //System.out.println("Замена sym2");
+                        if (checkWin(sym2)) {
+                            return;
+                        } else {
+                            desk[i][j] = ' ';
+                            //System.out.println("Замена на пробел1");
+                        }
+                        desk[i][j] = sym1;
+                        //System.out.println("Замена на sym1");
+                        if (checkWin(sym1)) {
+                            desk[i][j] = sym2;
+                            return;
+                        } else {
+                            desk[i][j] = ' ';
+                            //System.out.println("Замена на пробел2");
+                        }
+                    }
+                }
+            }
+            while (true){
+                x2 = (int)(Math.random()*3);
+                y2 = (int)(Math.random()*3);
+                if ((desk[x2][y2] != sym1) && (desk[x2][y2] != sym2)){
+                    desk[x2][y2] = sym2;
+                    //System.out.println("Рандом");
+                    break;
+                }
+                if (checkDraw()){
+                    break;
+                }
+            }
+        }
+
     }
 
     public static boolean firstMove(){
-        boolean flag;
         //Scanner inp = new Scanner(System.in);
         System.out.println("Будете ходить первым?");
         System.out.println("Y - \"Yes\", N - \"No\"");
         while (true){
             String scan = SCANNER.nextLine();
             if (scan.equals("Y")){
-                flag = true;
-                break;
+                return true;
             } else if (scan.equals("N")){
-                flag = false;
-                break;
+                return false;
             } else{
                 System.out.println("Введите Y или N");
             }
         }
-
-        return flag;
     }
 
     public static void printDesk(){
@@ -150,30 +119,38 @@ public class Test {
         System.out.println("---------");
     }
 
-    public static void playerTurn(){
+    public static void playerTurn(char sym){
         //Scanner s = new Scanner(System.in);
         System.out.println("Введите координату поля");
         int coordinate = parseInt(SCANNER.nextLine().replace(" ", ""));
         //System.out.println(coordinate);
         int x1 = coordinate / 10;
         int y1 = coordinate % 10;
-        desk[3-x1][y1-1] = 'X';
+        desk[3-x1][y1-1] = sym;
 
     }
     public static void main(String[] args) {
-        //boolean flag1 = firstMove();
-        //int ord = 1;
+        char symPlayer = 'X';
+        char symBot = '0';
         fillingDesk();
-        while (checkWin()){
-            playerTurn();
-            botTurn();
-            if (!checkDraw()){
+        while(true) {
+            playerTurn(symPlayer);
+            if (checkWin(symPlayer)) {
+                System.out.println("Победил игрок");
+                break;
+            }
+            botTurn(symPlayer, symBot);
+            printDesk();
+            if (checkWin(symBot)) {
+                System.out.println("Победил компьютер");
+                break;
+            }
+            if (checkDraw()) {
                 System.out.println("Ничья");
                 break;
             }
         }
-        //TODO вывод победителя
-        System.out.println("GAME OVER!");
+        System.out.println("Игра окончена");
         printDesk();
     }
 }
